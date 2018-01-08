0a02baa1c3ee36ca563391da63f1ae11143d4123
Mike <miguelpelmedina@gmail.com>
2018-01-08 23:08:05 +0100

    Ajuste de la pantalla Inicio

--- src/interfaz/IntfzPantallaInicio.java
+++ src/interfaz/IntfzPantallaInicio.java
@@ -149,23 +149,23 @@ public class IntfzPantallaInicio extends JFrame {
 		textField_kappa.setColumns(10);
 
 		JLabel lblNombre = new JLabel("Nombre:");
-		lblNombre.setBounds(10, 11, 46, 14);
+		lblNombre.setBounds(10, 11, 99, 14);
 		panelEditar.add(lblNombre);
 
 		JLabel lblAlpha = new JLabel("Alpha:");
-		lblAlpha.setBounds(10, 36, 46, 14);
+		lblAlpha.setBounds(10, 36, 99, 14);
 		panelEditar.add(lblAlpha);
 
 		JLabel lblBeta = new JLabel("Beta:");
-		lblBeta.setBounds(10, 61, 46, 14);
+		lblBeta.setBounds(10, 61, 99, 14);
 		panelEditar.add(lblBeta);
 
 		JLabel lblGamma = new JLabel("Gamma:");
-		lblGamma.setBounds(10, 86, 46, 14);
+		lblGamma.setBounds(10, 86, 99, 14);
 		panelEditar.add(lblGamma);
 
 		JLabel lblKappa = new JLabel("Kappa:");
-		lblKappa.setBounds(10, 111, 46, 14);
+		lblKappa.setBounds(10, 111, 99, 14);
 		panelEditar.add(lblKappa);
 
 		JButton btnGuardar = new JButton("Guardar");
@@ -226,7 +226,7 @@ public class IntfzPantallaInicio extends JFrame {
 		panelEditar.add(textField_tecnologia);
 
 		JLabel lblTecnologia = new JLabel("tecnologia:");
-		lblTecnologia.setBounds(10, 136, 71, 14);
+		lblTecnologia.setBounds(10, 136, 99, 14);
 		panelEditar.add(lblTecnologia);
 
 		lblTemperaturaNocturna = new JLabel("Temperatura NOCT:");
@@ -239,7 +239,7 @@ public class IntfzPantallaInicio extends JFrame {
 		panelEditar.add(textField_tempNOCT);
 
 		lblIscNOCT = new JLabel("ISC NOCT:");
-		lblIscNOCT.setBounds(10, 186, 71, 14);
+		lblIscNOCT.setBounds(10, 186, 99, 14);
 		panelEditar.add(lblIscNOCT);
 
 		textField_iscNOCT = new JTextField();
@@ -257,7 +257,7 @@ public class IntfzPantallaInicio extends JFrame {
 		panelEditar.add(textField_vocNOCT);
 
 		lblpmaxNOCT = new JLabel("PMAX NOCT:");
-		lblpmaxNOCT.setBounds(10, 236, 71, 14);
+		lblpmaxNOCT.setBounds(10, 236, 89, 14);
 		panelEditar.add(lblpmaxNOCT);
 
 		textField_pmaxNOCT = new JTextField();
@@ -266,7 +266,7 @@ public class IntfzPantallaInicio extends JFrame {
 		panelEditar.add(textField_pmaxNOCT);
 
 		lvlIpmax_NOCT = new JLabel("IPMAX NOCT:");
-		lvlIpmax_NOCT.setBounds(10, 261, 71, 14);
+		lvlIpmax_NOCT.setBounds(10, 261, 99, 14);
 		panelEditar.add(lvlIpmax_NOCT);
 
 		textField_ipmaxNOCT = new JTextField();
@@ -275,7 +275,7 @@ public class IntfzPantallaInicio extends JFrame {
 		panelEditar.add(textField_ipmaxNOCT);
 
 		lblVpmax = new JLabel("VPMAX NOCT:");
-		lblVpmax.setBounds(10, 286, 86, 14);
+		lblVpmax.setBounds(10, 286, 99, 14);
 		panelEditar.add(lblVpmax);
 
 		textField_vpmaxNOCT = new JTextField();
@@ -284,94 +284,94 @@ public class IntfzPantallaInicio extends JFrame {
 		panelEditar.add(textField_vpmaxNOCT);
 
 		lblRendimiento = new JLabel("Rendimiento:");
-		lblRendimiento.setBounds(238, 36, 71, 14);
+		lblRendimiento.setBounds(238, 36, 139, 14);
 		panelEditar.add(lblRendimiento);
 
 		textField_rendimiento = new JTextField();
 		textField_rendimiento.setColumns(10);
-		textField_rendimiento.setBounds(352, 33, 86, 20);
+		textField_rendimiento.setBounds(403, 33, 86, 20);
 		panelEditar.add(textField_rendimiento);
 
 		lblIdealidad = new JLabel("Idealidad:");
-		lblIdealidad.setBounds(238, 61, 71, 14);
+		lblIdealidad.setBounds(238, 61, 139, 14);
 		panelEditar.add(lblIdealidad);
 
 		lblResistencia = new JLabel("Resistencia:");
-		lblResistencia.setBounds(238, 86, 71, 14);
+		lblResistencia.setBounds(238, 86, 139, 14);
 		panelEditar.add(lblResistencia);
 
 		lblMinIsc = new JLabel("min ISC");
-		lblMinIsc.setBounds(238, 111, 59, 14);
+		lblMinIsc.setBounds(238, 111, 139, 14);
 		panelEditar.add(lblMinIsc);
 
 		lblMinVOC = new JLabel("min VOC:");
-		lblMinVOC.setBounds(238, 136, 71, 14);
+		lblMinVOC.setBounds(238, 136, 139, 14);
 		panelEditar.add(lblMinVOC);
 
 		lblMinPmax = new JLabel("min PMAX:");
-		lblMinPmax.setBounds(238, 161, 71, 14);
+		lblMinPmax.setBounds(238, 161, 139, 14);
 		panelEditar.add(lblMinPmax);
 
 		lblMinFf = new JLabel("min FF:");
-		lblMinFf.setBounds(238, 186, 46, 14);
+		lblMinFf.setBounds(238, 186, 139, 14);
 		panelEditar.add(lblMinFf);
 
 		lblCelulasEnSerie = new JLabel("celulas en serie:");
-		lblCelulasEnSerie.setBounds(238, 211, 86, 14);
+		lblCelulasEnSerie.setBounds(238, 211, 139, 14);
 		panelEditar.add(lblCelulasEnSerie);
 
 		lblCelulasEnParalelo = new JLabel("celulas en paralelo:");
-		lblCelulasEnParalelo.setBounds(238, 236, 99, 14);
+		lblCelulasEnParalelo.setBounds(238, 236, 139, 14);
 		panelEditar.add(lblCelulasEnParalelo);
 
 		textField_celulasEnParalelo = new JTextField();
 		textField_celulasEnParalelo.setColumns(10);
-		textField_celulasEnParalelo.setBounds(352, 233, 86, 20);
+		textField_celulasEnParalelo.setBounds(403, 233, 86, 20);
 		panelEditar.add(textField_celulasEnParalelo);
 
 		textField_celulasEnSerie = new JTextField();
 		textField_celulasEnSerie.setColumns(10);
-		textField_celulasEnSerie.setBounds(352, 208, 86, 20);
+		textField_celulasEnSerie.setBounds(403, 208, 86, 20);
 		panelEditar.add(textField_celulasEnSerie);
 
 		textField_minFF = new JTextField();
 		textField_minFF.setColumns(10);
-		textField_minFF.setBounds(352, 183, 86, 20);
+		textField_minFF.setBounds(403, 183, 86, 20);
 		panelEditar.add(textField_minFF);
 
 		textField_minPMAX = new JTextField();
 		textField_minPMAX.setColumns(10);
-		textField_minPMAX.setBounds(352, 158, 86, 20);
+		textField_minPMAX.setBounds(403, 158, 86, 20);
 		panelEditar.add(textField_minPMAX);
 
 		textField_minVOC = new JTextField();
 		textField_minVOC.setColumns(10);
-		textField_minVOC.setBounds(352, 133, 86, 20);
+		textField_minVOC.setBounds(403, 133, 86, 20);
 		panelEditar.add(textField_minVOC);
 
 		textField_minISC = new JTextField();
 		textField_minISC.setColumns(10);
-		textField_minISC.setBounds(352, 108, 86, 20);
+		textField_minISC.setBounds(403, 108, 86, 20);
 		panelEditar.add(textField_minISC);
 
 		textField_resistencia = new JTextField();
 		textField_resistencia.setColumns(10);
-		textField_resistencia.setBounds(352, 83, 86, 20);
+		textField_resistencia.setBounds(403, 83, 86, 20);
 		panelEditar.add(textField_resistencia);
 
 		textField_idealidad = new JTextField();
 		textField_idealidad.setColumns(10);
-		textField_idealidad.setBounds(352, 58, 86, 20);
+		textField_idealidad.setBounds(403, 58, 86, 20);
 		panelEditar.add(textField_idealidad);
 		panelEditar.setVisible(false);
 
 		JPanel panelInicial = new JPanel();
-		panelInicial.setBounds(0, 0, 575, 336);
+		panelInicial.setBounds(0, 0, 575, 381);
 		contentPane.add(panelInicial);
 		panelInicial.setLayout(null);
 		//------TEXTO DE ABAJO		
 		JTextPane txtpnBalBlaBla = new JTextPane();
-		txtpnBalBlaBla.setBounds(188, 81, 161, 23);
+		txtpnBalBlaBla.setBounds(191, 83, 161, 23);
 		panelInicial.add(txtpnBalBlaBla);
 		txtpnBalBlaBla.setBackground(UIManager.getColor("Button.background"));
 		txtpnBalBlaBla.setText("Bienvenido a PVTranslator");
@@ -415,7 +415,7 @@ public class IntfzPantallaInicio extends JFrame {
 
 		//-----TITULO
 		lblPvtranslator = new JLabel("PVTRANSLATOR");
-		lblPvtranslator.setBounds(134, 6, 291, 85);
+		lblPvtranslator.setBounds(134, 6, 292, 85);
 		panelInicial.add(lblPvtranslator);
 		lblPvtranslator.setFont(new Font("Times New Roman", Font.PLAIN, 35));
 		//-----IMAGEN LOGO
@@ -438,42 +438,9 @@ public class IntfzPantallaInicio extends JFrame {
 		ListaModuloTitulo.setBounds(10, 0, 414, 42);
 		panelModulo.add(ListaModuloTitulo);
 
-		button = new JButton("Atr\u00E1s");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				panelModulo.setVisible(false);
-				panelInicial.setVisible(true);
-			}
-		});
-		button.setBounds(10, 258, 89, 23);
-		panelModulo.add(button);
-
-
-		Elegir = new JButton("Elegir");
-		Elegir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-
-					if (list.getSelectedValue() != null) {
-						IntfzModuloElegido me = new IntfzModuloElegido(list.getSelectedValue());
-						me.newScreen(list.getSelectedValue());
-					}
-
-				} catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				//-------
-			}
-		});
-
-
-		Elegir.setBounds(430, 258, 89, 23);
-		panelModulo.add(Elegir);
-
 		list = new JList<String>();
 		list.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		list.setBounds(20, 46, 475, 180);
+		list.setBounds(20, 46, 545, 279);
 		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);    //solo se puede seleccionar uno
 
 		//creamos el modelo
@@ -489,51 +456,82 @@ public class IntfzPantallaInicio extends JFrame {
 
 		list.setModel(modeloCamp);
 		panelModulo.add(list);
+		
+				button = new JButton("Atr\u00E1s");
+				button.setBounds(10, 347, 89, 23);
+				contentPane.add(button);
+				
+				
+						Elegir = new JButton("Elegir");
+						Elegir.setBounds(466, 347, 89, 23);
+						contentPane.add(Elegir);
+						
+								JButton btnEditar = new JButton("Editar");
+								btnEditar.setBounds(367, 347, 89, 23);
+								contentPane.add(btnEditar);
+								btnEditar.addActionListener(new ActionListener() {
+									public void actionPerformed(ActionEvent e) {
+										if(list.getSelectedValue() != null) {
+											try {
 
-		JButton btnEditar = new JButton("Editar");
-		btnEditar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(list.getSelectedValue() != null) {
-					try {
+												Modulo mod = new Modulo(list.getSelectedValue());
+												//para que se vea el valor al entrar
 
-						Modulo mod = new Modulo(list.getSelectedValue());
-						//para que se vea el valor al entrar
-
-						//textField_nombre.setText(mod.getNombre());
-						textField_alpha.setText(String.valueOf(mod.getAlfa()));
-						textField_beta.setText(String.valueOf(mod.getBeta()));
-						textField_gamma.setText(String.valueOf(mod.getGamma()));
-						textField_kappa.setText(String.valueOf(mod.getKappa()));
-						textField_tecnologia.setText(String.valueOf(mod.getTecnologia()));
-						textField_tempNOCT.setText(String.valueOf(mod.getTemperaturaNOCT()));
-						textField_iscNOCT.setText(String.valueOf(mod.getIscNOCT()));
-						textField_vocNOCT.setText(String.valueOf(mod.getVocNOCT()));
-						textField_pmaxNOCT.setText(String.valueOf(mod.getPmaxNOCT()));
-						textField_ipmaxNOCT.setText(String.valueOf(mod.getIpmaxNOCT()));
-						textField_vpmaxNOCT.setText(String.valueOf(mod.getVpmaxNOCT()));
-						textField_rendimiento.setText(String.valueOf(mod.getRendimiento()));
-						textField_idealidad.setText(String.valueOf(mod.getIdealidad()));
-						textField_resistencia.setText(String.valueOf(mod.getResistencia()));
-						textField_minISC.setText(String.valueOf(mod.getMinISC()));
-						textField_minVOC.setText(String.valueOf(mod.getMinVOC()));
-						textField_minPMAX.setText(String.valueOf(mod.getMinPMAX()));
-						textField_minFF.setText(String.valueOf(mod.getMinFF()));
-						textField_celulasEnSerie.setText(String.valueOf(mod.getCelSerie()));
-						textField_celulasEnParalelo.setText(String.valueOf(mod.getCelParalelo()));
+												//textField_nombre.setText(mod.getNombre());
+												textField_alpha.setText(String.valueOf(mod.getAlfa()));
+												textField_beta.setText(String.valueOf(mod.getBeta()));
+												textField_gamma.setText(String.valueOf(mod.getGamma()));
+												textField_kappa.setText(String.valueOf(mod.getKappa()));
+												textField_tecnologia.setText(String.valueOf(mod.getTecnologia()));
+												textField_tempNOCT.setText(String.valueOf(mod.getTemperaturaNOCT()));
+												textField_iscNOCT.setText(String.valueOf(mod.getIscNOCT()));
+												textField_vocNOCT.setText(String.valueOf(mod.getVocNOCT()));
+												textField_pmaxNOCT.setText(String.valueOf(mod.getPmaxNOCT()));
+												textField_ipmaxNOCT.setText(String.valueOf(mod.getIpmaxNOCT()));
+												textField_vpmaxNOCT.setText(String.valueOf(mod.getVpmaxNOCT()));
+												textField_rendimiento.setText(String.valueOf(mod.getRendimiento()));
+												textField_idealidad.setText(String.valueOf(mod.getIdealidad()));
+												textField_resistencia.setText(String.valueOf(mod.getResistencia()));
+												textField_minISC.setText(String.valueOf(mod.getMinISC()));
+												textField_minVOC.setText(String.valueOf(mod.getMinVOC()));
+												textField_minPMAX.setText(String.valueOf(mod.getMinPMAX()));
+												textField_minFF.setText(String.valueOf(mod.getMinFF()));
+												textField_celulasEnSerie.setText(String.valueOf(mod.getCelSerie()));
+												textField_celulasEnParalelo.setText(String.valueOf(mod.getCelParalelo()));
 
 
+												panelModulo.setVisible(false);
+												panelEditar.setVisible(true);
+
+											} catch (ClassNotFoundException e1) {
+												// TODO Auto-generated catch block
+												e1.printStackTrace();
+											}
+										}
+									}
+								});
+						Elegir.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								try {
+
+									if (list.getSelectedValue() != null) {
+										IntfzModuloElegido me = new IntfzModuloElegido(list.getSelectedValue());
+										me.newScreen(list.getSelectedValue());
+									}
+
+								} catch (ClassNotFoundException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+								//-------
+							}
+						});
+				button.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
 						panelModulo.setVisible(false);
-						panelEditar.setVisible(true);
-
-					} catch (ClassNotFoundException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
+						panelInicial.setVisible(true);
 					}
-				}
-			}
-		});
-		btnEditar.setBounds(326, 258, 89, 23);
-		panelModulo.add(btnEditar);
+				});
 
 
 		panelModulo.setVisible(false);
